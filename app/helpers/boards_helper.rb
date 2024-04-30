c0c86de6ab0085a72a37ae004413ff87591b9fdd
Sam Williams <sam.williams@brazen.com>
2024-04-30 06:59:57 -0400

    Board styling

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -9,6 +9,29 @@ module BoardsHelper
     set.intersection(near_array).count
   end
 
+  def bombs_near_color(set, x, y)
+    case bombs_near(set, x, y)
+    when 1
+      "text-blue-700"
+    when 2
+      "text-green-700"
+    when 3
+      "text-red-700"
+    when 4
+      "text-indigo-900"
+    when 5
+      "text-orange-800"
+    when 6
+      "text-teal-600"
+    when 7
+      "text-black"
+    when 8
+      "text-gray-500"
+    else
+      "text-lime-500"
+    end
+  end
+
   def click_path(board, x, y)
     "#{board_click_path(board, x: x, y: y)}"
   end
